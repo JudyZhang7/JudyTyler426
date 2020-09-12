12f8e30f89c8c4b8efa40dde4878a1e5546d7300
Judy Zhang <judyzhang98@gmail.com>
2020-09-12 15:08:07 -0700

    skydome

--- Assets/Scripts/TempPlayer.cs
+++ Assets/Scripts/TempPlayer.cs
@@ -6,15 +6,29 @@ using UnityEngine.UI;
 public class TempPlayer : MonoBehaviour
 {
     public Slider slider;
+    // control environment
+    public GameObject skyDome;
+    private Renderer rend;
+    private float domeTime;
+    private float startDomeTime = 0f; //6.66
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rend = skyDome.GetComponent<Renderer>();
+        domeTime = startDomeTime;
     }
 
     public float speed = 20f;
  
      void Update () {
+         // skydome
+        float offset = domeTime * 0.03f * 0.5f;
+        domeTime += Time.deltaTime;
+        if (offset < 0.43){
+            rend.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+        }
+
          Vector3 pos = transform.position;
  
          if (Input.GetKey ("w")) {
