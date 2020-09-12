b8a9a8921e69bd69c5c9fcaf5672faa9780afeb2
Tyler Amano-Smerling <amanosme@usc.edu>
2020-09-12 16:31:46 -0700

    added in camera movement and ghost mat

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void MoveCamera()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = -Camera.main.transform.position.z;
+        Vector3 inputPos = Camera.main.ScreenToWorldPoint(mousePos);
+        Camera.main.transform.LookAt(inputPos - Camera.main.transform.position);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        MoveCamera();   
+    }
+}
