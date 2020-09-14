0c52f289c0cec44c97f03ee29d26018d61fcf232
Tyler Amano-Smerling <amanosme@usc.edu>
2020-09-13 17:12:04 -0700

    made end screen

---
+++ Assets/Scripts/EndScreen.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndScreen : MonoBehaviour
+{
+    public Vector4 color;
+    SpriteRenderer spriteRenderer;
+    float timer = -1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        color.w = 0;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.color = color;
+    }
+
+    public void StartTimer()
+    {
+        timer = 0;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer >= 0) timer += Time.deltaTime;
+        if (color.w < 1)
+        {
+            color.w += Time.deltaTime*0.5f;
+            spriteRenderer.color = color;
+        }
+        else if (timer > 5) SceneManager.LoadScene(0);
+        //if (timer > 3) SceneManager.LoadScene(0);
+    }
+}
